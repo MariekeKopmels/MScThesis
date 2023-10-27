f9a30efc1fc9f433110482902bae849e20cc4053
Marieke Kopmels <marieke@kopmels.com>
2023-10-27 15:06:25 +0200

    Add author

--- SkinSegmentation.py
+++ SkinSegmentation.py
@@ -1,3 +1,5 @@
+#MSc Thesis Marieke Kopmels
+
 import time
 import os
 import numpy as np
@@ -7,10 +9,12 @@ from torch.nn import Linear
 from torch.nn import MaxPool2d
 from torch.nn import ReLU
 from torch.nn import LogSoftmax
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import torch.utils.data
+from torchvision import transforms
 from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights
-from PIL import Image
+# from PIL import Image
 from torch import optim
 from torch import flatten
 from os import listdir
@@ -21,15 +25,15 @@ import torch.utils.data
 
 # LET OP: Batch size moet devidable by 60 zijn, anders gaat output = x.reshape(6,2,224,224) niet goed bij de laatste batch.
 BATCH_SIZE = 20
-NUM_EPOCHS = 20
+NUM_EPOCHS = 2
 LR=0.05
-MOMENTUM = 0.99
+MOMENTUM = 0.9
 
 """Returns images in a given directory
         Format of return: list of NumPy arrays.
         NumPy arrays are of shape (224,224,3) 
 """
-def load_images(data, dir_path):
+def load_images(data, dir_path, gt=False):
     # Load list of files and directories
     data_list = listdir(dir_path)
     data_list = sorted(data_list, key=str.casefold)
@@ -41,6 +45,9 @@ def load_images(data, dir_path):
         # Files
         img_path = dir_path + "/" + img_name
         img = cv2.imread(img_path)
+        if gt:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret,img = cv2.threshold(img,70,255,0)
         img = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
         data.append(img)
     return data
@@ -55,15 +62,25 @@ def load_data(train, test):
 
     train_images, train_gt, test_images, test_gt = [], [], [], []
     train_images = load_images(train_images, base_path + "/TrainData")
-    train_gt = load_images(train_gt, base_path + "/TrainGroundTruth")
+    train_gt = load_images(train_gt, base_path + "/TrainGroundTruth", gt=True)
     test_images = load_images(test_images, base_path + "/TestData")
-    test_gt = load_images(test_gt, base_path + "/TestGroundTruth")
+    test_gt = load_images(test_gt, base_path + "/TestGroundTruth", gt=True)
+    
+    # print("TESTING IMAGE LOADING")
+    # cv2.namedWindow("imagename")
+    # cv2.imshow('imagename',train_images[0])
+    # cv2.waitKey(0)
+    # cv2.namedWindow("imagename")
+    # cv2.imshow('imagename',train_gt[0])
+    # cv2.waitKey(0)
+    # print(f"torch.as_tensor(np.array(train_gt)) = {torch.as_tensor(np.array(train_gt)).shape}")
+    
     # test_images = load_images(test_images, base_path + "/MockTrainData")
     # test_gt = load_images(test_gt, base_path + "/MockTrainGroundTruth")
-        
+
     # TODO: fix de eerst naar numpy en daarna pas naar tensor (sneller dan vanaf een list direct naar tensor maar nu heel lelijk)
-    train = torch.utils.data.TensorDataset(torch.as_tensor(np.array(train_images)).permute(0,3,1,2), torch.as_tensor(np.array(train_gt)).permute(0,3,1,2))
-    test = torch.utils.data.TensorDataset(torch.as_tensor(np.array(test_images)).permute(0,3,1,2), torch.as_tensor(np.array(test_gt)).permute(0,3,1,2))
+    train = torch.utils.data.TensorDataset(torch.as_tensor(np.array(train_images)).permute(0,3,1,2), torch.as_tensor(np.array(train_gt)).permute(0,1,2))
+    test = torch.utils.data.TensorDataset(torch.as_tensor(np.array(test_images)).permute(0,3,1,2), torch.as_tensor(np.array(test_gt)).permute(0,1,2))
 
     return train, test
 
@@ -73,22 +90,22 @@ class LeNet(Module):
         super(LeNet, self).__init__()
 
         # initialize first set of CONV => RELU => POOL layers
-        self.conv1 = Conv2d(in_channels=numChannels, out_channels=10,kernel_size=(5, 5))
+        self.conv1 = Conv2d(in_channels=numChannels, out_channels=10,kernel_size=(3, 3))
         self.relu1 = ReLU()
         self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
         # initialize second set of CONV => RELU => POOL layers
-        self.conv2 = Conv2d(in_channels=10, out_channels=20,kernel_size=(5, 5))
+        self.conv2 = Conv2d(in_channels=10, out_channels=20,kernel_size=(3, 3))
         self.relu2 = ReLU()
         self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         
         # initialize first (and only) set of FC => RELU layers
-        self.fc1 = Linear(in_features=20*53*53, out_features=1000)
+        self.fc1 = Linear(in_features=20*54*54, out_features=1000)
         self.relu3 = ReLU()
         
         # initialize our softmax classifier
         #TODO: Wil ik jpeg (dus RBG) voorspellen of skin/non-skin voorspellen en dan JPEG ground truths naar 2D formatten
-        self.fc2 = Linear(in_features=1000, out_features=224*224*3)
+        self.fc2 = Linear(in_features=1000, out_features=224*224)
         self.logSoftmax = LogSoftmax(dim=1)
         
     def forward(self, x):
@@ -116,7 +133,7 @@ class LeNet(Module):
         x = self.logSoftmax(x)
         
         # TODO: Fixen dat dit niet hardcoded is
-        output = x.reshape(BATCH_SIZE,3,224,224)
+        output = x.reshape(BATCH_SIZE,224,224)
         
         # return the output predictions
         return output
@@ -131,8 +148,8 @@ if __name__ == '__main__':
     
     # This should allow for it to run on M1 GPU
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("mps")
-    model.to(device)
+    # device = torch.device("mps")
+    # model.to(device)
     
     # Initialize optimizer and learining rate scheduler
     # params = [p for p in model.parameters() if p.requires_grad]
@@ -148,6 +165,8 @@ if __name__ == '__main__':
     train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
     test_loader = DataLoader(test, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
+    # i = 0 
+    
     #Train the model
     for epoch in range(NUM_EPOCHS):
         print("-------------------------New Epoch-------------------------")
@@ -155,15 +174,15 @@ if __name__ == '__main__':
         epoch_train_loss = 0.0
         epoch_test_loss = 0.0
         for images, targets in train_loader:
-            # print("-------------------------New Batch-------------------------")
-            list_images = list(image for image in images)
-            list_targets = list(target for target in targets)
+            # list_images = list(image for image in images)
+            # list_targets = list(target for target in targets)
             
-            images = images.to(device)
+            # images = images.to(device)
             images = images.float()
-            targets = targets.to(device)
+            # targets = targets.to(device)
             targets = targets.float()
-                    
+            
+            # Train the model
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, targets)
@@ -173,28 +192,44 @@ if __name__ == '__main__':
             epoch_train_loss += loss.item()
             
         for images, targets in test_loader:
-            list_images = list(image for image in images)
-            list_targets = list(target for target in targets)
-            images = images.to(device)
+            # list_images = list(image for image in images)
+            # list_targets = list(target for target in targets)
+            # images = images.to(device)
             images = images.float()
-            targets = targets.to(device)
+            # targets = targets.to(device)
             targets = targets.float()
             
             loss = criterion(outputs, targets)
             epoch_test_loss += loss.item()
+            
+            # if i == 0:
+            #     img = images[0]
+            #     # img.cpu()
+            #     img = img.permute(1,2,0).numpy()
+            #     # img.to(device)
+            #     print("dims(image): ", img.shape)
+            #     cv2.namedWindow("imagename")
+            #     cv2.imshow('imagename',img)
+            #     cv2.waitKey(0)
+                
+            # i += 1
         
         
         # Print losses TODO: is the /X factor correct? 
-        print(f"[{epoch + 1}] \ntrain loss: {epoch_train_loss/60:.6f} \ntest  loss: {epoch_test_loss/20:.6f} ")
+        print(f"[{epoch + 1}] \ntrain loss: {epoch_train_loss:.6f} \ntest  loss: {epoch_test_loss:.6f} ")
+    
+    # test_path = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Datasets/Pratheepan_Dataset/MockTrainData/Image1.jpeg"
+    # img = cv2.imread(test_path)
+    # img = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
+    # cv2.namedWindow("imagename")
+    # cv2.imshow('imagename',img)
+    # cv2.waitKey(0)
+    # data = []
+    # data.append(img)
+    # data = torch.as_tensor(np.array(data)).permute(0,3,1,2)
+    # model(img)
         
             
     run_time = time.time() - start_time
     print("Running time: ", round(run_time,3))
             
-    
-
-
-
-
-
-
