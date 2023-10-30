1601a1cba329b8e860fef0d97f81b55858f44c86
Marieke Kopmels <marieke@kopmels.com>
2023-10-30 13:24:37 +0100

    Updated batch size issue, using new data

--- SkinSegmentation.py
+++ SkinSegmentation.py
@@ -22,8 +22,8 @@ from torch.utils.data import DataLoader
 # from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights
 
 # LET OP: Batch size moet dividable by 60 zijn, anders gaat output = x.reshape(BATCH_SIZE,224,224) niet goed bij de laatste batch.
-BATCH_SIZE = 16
-NUM_EPOCHS = 5
+BATCH_SIZE = 256
+NUM_EPOCHS = 50
 LR=0.01
 MOMENTUM = 0.9
 
@@ -31,44 +31,65 @@ MOMENTUM = 0.9
         Format of return: list of NumPy arrays.
         NumPy arrays are of shape (224,224,3) for images and (224,224) for ground truths 
 """
-def load_images(data, dir_path, gt=False):
+def load_images(images, gts, image_dir_path, gt_dir_path, test=False):
     # Load list of files and directories
-    data_list = listdir(dir_path)
-    data_list = sorted(data_list, key=str.casefold)
+    image_list = listdir(image_dir_path)
+    gt_list = listdir(gt_dir_path)
     
-    for img_name in data_list:
+    # Not all images have a ground truth, select those that do
+    dir_list = [file for file in image_list if file in gt_list]
+    dir_list = sorted(dir_list, key=str.casefold)
+    
+    if not test:
+        dir_list = dir_list[:5000]
+    
+    for file_name in dir_list:
         # Hidden files, irrelevant for this usecase
-        if img_name.startswith('.'):
+        if file_name.startswith('.'):
             continue
-        # Files
-        img_path = dir_path + "/" + img_name
+        # Read the images
+        img_path = image_dir_path + "/" + file_name
+        gt_path = gt_dir_path + "/" + file_name
+        
         img = cv2.imread(img_path)
-        if gt:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            ret,img = cv2.threshold(img,70,255,0)
+        gt = cv2.imread(gt_path)
+        
+        # Convert Ground Truth from RGB to Black or White
+        gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY)
+        ret,gt = cv2.threshold(gt,70,255,0)
+        
+        # Resize images and ground truths to size 224*224
         img = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
-        data.append(img)
-    return data
+        gt = cv2.resize(gt, (224,224), interpolation=cv2.INTER_CUBIC)
+        
+        #Store in list
+        images.append(img)
+        gts.append(gt)
+        
+    return images, gts
 
 """Returns train and test, both being tensors with tuples of input images and corresponding ground truths.
         Format of return: Tensor of tuples containging an input tensor and a ground truth tensor
         Image tensors are of shape (batch_size, channels, height, width)
 """
 def load_data(train, test):
-    base_path = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Datasets/Pratheepan_Dataset"
+    base_path = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Datasets/visuAAL"
     device = torch.device("mps")
 
-    train_images, train_gt, test_images, test_gt = [], [], [], []
-    train_images = load_images(train_images, base_path + "/TrainData")
-    train_gt = load_images(train_gt, base_path + "/TrainGroundTruth", gt=True)
-    test_images = load_images(test_images, base_path + "/TestData")
-    test_gt = load_images(test_gt, base_path + "/TestGroundTruth", gt=True)
+    train_images, train_gts, test_images, test_gts = [], [], [], []
+    print("Loading training data...")
+    train_images, train_gts = load_images(train_images, train_gts, base_path + "/TrainImages", base_path + "/TrainGroundTruth")
+    # train_gt = load_images(train_gt, base_path + "/TrainGroundTruth", gt=True)
+    print("Loading testing data...")
+    test_images, test_gts = load_images(test_images, test_gts, base_path + "/TestImages", base_path + "/TestGroundTruth", test = True)
+    # test_gt = load_images(test_gt, base_path + "/TestGroundTruth", gt=True)
     
-    
+    # print(f"train_images.shape: {len(train_images)}, train_gts.shape: {len(train_gts)}")
+    # print(f"train_images.shape: {len(test_images)}, train_gts.shape: {len(test_gts)}")
     
     # TODO: fix de eerst naar numpy en daarna pas naar tensor (sneller dan vanaf een list direct naar tensor maar nu heel lelijk)
-    train = torch.utils.data.TensorDataset(torch.as_tensor(np.array(train_images)).permute(0,3,1,2), torch.as_tensor(np.array(train_gt)).permute(0,1,2))
-    test = torch.utils.data.TensorDataset(torch.as_tensor(np.array(test_images)).permute(0,3,1,2), torch.as_tensor(np.array(test_gt)).permute(0,1,2))
+    train = torch.utils.data.TensorDataset(torch.as_tensor(np.array(train_images)).permute(0,3,1,2), torch.as_tensor(np.array(train_gts)).permute(0,1,2))
+    test = torch.utils.data.TensorDataset(torch.as_tensor(np.array(test_images)).permute(0,3,1,2), torch.as_tensor(np.array(test_gts)).permute(0,1,2))
 
     return train, test
 
@@ -158,6 +179,10 @@ if __name__ == '__main__':
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+    
+    # Store the losses
+    train_losses = []
+    test_losses = []
 
     # Load data, both input images and ground truths. Put them into a DataLoader
     train, test = load_data([], [])
@@ -172,9 +197,7 @@ if __name__ == '__main__':
         model.train()
         epoch_train_loss = 0.0
         epoch_test_loss = 0.0
-        for images, targets in train_loader:
-            print("-------------------------New Batch-------------------------")
-            
+        for images, targets in train_loader:            
             images = images.to(device)
             images = images.float()
             targets = targets.to(device)
@@ -201,18 +224,9 @@ if __name__ == '__main__':
             epoch_test_loss += loss.item()
         
         # Print losses TODO: is the /X factor correct? 
+        train_losses.append(epoch_train_loss/train_dataset_size)
+        test_losses.append(epoch_test_loss/test_dataset_size)
         print(f"[{epoch + 1}] \ntrain loss: {epoch_train_loss/train_dataset_size:.6f} \ntest  loss: {epoch_test_loss/test_dataset_size:.6f} ")
-    
-    # test_path = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Datasets/Pratheepan_Dataset/MockTrainData/Image1.jpeg"
-    # img = cv2.imread(test_path)
-    # img = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
-    # cv2.namedWindow("imagename")
-    # cv2.imshow('imagename',img)
-    # cv2.waitKey(0)
-    # data = []
-    # data.append(img)
-    # data = torch.as_tensor(np.array(data)).permute(0,3,1,2)
-    # model(img)
             
     run_time = time.time() - start_time
     print("Running time: ", round(run_time,3))
