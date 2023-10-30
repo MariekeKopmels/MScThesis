b7fc6606a5e1a4ed62ce331cb56fea1046dfe826
Marieke Kopmels <marieke@kopmels.com>
2023-10-30 15:38:32 +0100

    minor updates

--- SkinSegmentation.py
+++ SkinSegmentation.py
@@ -28,6 +28,7 @@ LR              = 0.01
 MOMENTUM        = 0.9
 TRAIN_SIZE      = 5000
 TEST_SIZE       = 500
+NO_PIXELS       = 224
 
 """Returns images in a given directory
         Format of return: list of NumPy arrays.
@@ -63,8 +64,8 @@ def load_images(images, gts, image_dir_path, gt_dir_path, test=False):
         ret,gt = cv2.threshold(gt,70,255,0)
         
         # Resize images and ground truths to size 224*224
-        img = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
-        gt = cv2.resize(gt, (224,224), interpolation=cv2.INTER_CUBIC)
+        img = cv2.resize(img, (NO_PIXELS,NO_PIXELS), interpolation=cv2.INTER_CUBIC)
+        gt = cv2.resize(gt, (NO_PIXELS,NO_PIXELS), interpolation=cv2.INTER_CUBIC)
         
         #Store in list
         images.append(img)
@@ -97,62 +98,8 @@ def load_data(train, test):
 
     return train, test
 
-class UNet(nn.Module):
-    def __init__(self, n_class):
-        super().__init__()
-        
-        # Encoder
-        # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
-        # Each block in the encoder consists of two convolutional layers followed by a max-pooling layer, with the exception of the last block which does not include a max-pooling layer.
-        # -------
-        # input: 572x572x3
-        self.e11 = nn.Conv2d(3, 64, kernel_size=3, padding=1) # output: 570x570x64
-        self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 568x568x64
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
-
-        # input: 284x284x64
-        self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 282x282x128
-        self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 280x280x128
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
-
-        # input: 140x140x128
-        self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 138x138x256
-        self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 136x136x256
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
-
-        # input: 68x68x256
-        self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) # output: 66x66x512
-        self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 64x64x512
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
-
-        # input: 32x32x512
-        self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1) # output: 30x30x1024
-        self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 28x28x1024
-
-
-        # Decoder
-        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-
-        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-
-        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-
-        # Output layer
-        self.outconv = nn.Conv2d(64, n_class, kernel_size=1)
-        
-
 class LeNet(Module):
-    def __init__(self, numChannels, classes):
+    def __init__(self, numChannels, num_pixels):
 		# call the parent constructor
         super(LeNet, self).__init__()
 
@@ -172,7 +119,7 @@ class LeNet(Module):
         
         # initialize our softmax classifier
         #TODO: Wil ik jpeg (dus RBG) voorspellen of skin/non-skin voorspellen en dan JPEG ground truths naar 2D formatten
-        self.fc2 = Linear(in_features=1000, out_features=1*224*224)
+        self.fc2 = Linear(in_features=1000, out_features=NO_PIXELS*NO_PIXELS)
         self.logSoftmax = LogSoftmax(dim=1)
         
     def forward(self, x):
@@ -216,7 +163,7 @@ class LeNet(Module):
         x = self.logSoftmax(x)
         
         # TODO: Checken of deze manier van batch_size werkt ipv hardcoded
-        output = x.reshape(batch_size,224,224)
+        output = x.reshape(batch_size,NO_PIXELS,NO_PIXELS)
         
         # return the output predictions
         return output
@@ -227,7 +174,7 @@ if __name__ == '__main__':
     start_time = time.time()
     # Create model
     # model = fcn_resnet50(weights=FCN_ResNet50_Weights.DEFAULT, num_channels=2)
-    model = LeNet(numChannels=3, classes=2)
+    model = LeNet(numChannels=3)
     
     # This should allow for it to run on M1 GPU
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -310,15 +257,15 @@ if __name__ == '__main__':
     # print("Ground truth")
     # print(gt)
     
-    cv2.namedWindow("demo_image")
-    cv2.imshow('demo_image', img)
-    cv2.waitKey(0)
-    cv2.namedWindow("demo_output")
-    cv2.imshow('demo_output',rgb_out)
-    cv2.waitKey(0)
-    cv2.namedWindow("demo_gt")
-    cv2.imshow('demo_gt',gt)
-    cv2.waitKey(0)
+    # cv2.namedWindow("demo_image")
+    # cv2.imshow('demo_image', img)
+    # cv2.waitKey(0)
+    # cv2.namedWindow("demo_output")
+    # cv2.imshow('demo_output',rgb_out)
+    # cv2.waitKey(0)
+    # cv2.namedWindow("demo_gt")
+    # cv2.imshow('demo_gt',gt)
+    # cv2.waitKey(0)
     
     # Plot losses
     x = range(1, NUM_EPOCHS + 1)
