a6b5d3e92d79d5263b8125c79c38777abfb3b204
Marieke Kopmels <marieke@kopmels.com>
2023-11-02 15:12:45 +0100

    multiple options for loss functions added

--- SkinSegmentation.py
+++ SkinSegmentation.py
@@ -9,28 +9,39 @@ import cv2
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import optim
 from torchmetrics.classification import Dice
 from torch.utils.data import DataLoader
 
-# Test
+
+# # Test
 BATCH_SIZE      = 32
 NUM_EPOCHS      = 25
-LR              = 0.001
-MOMENTUM        = 0.9
+LR              = 0.01
+MOMENTUM        = 0.99
 TRAIN_SIZE      = 128
-TEST_SIZE       = 16
+TEST_SIZE       = 32
 NO_PIXELS       = 224
 
 # Real
-# BATCH_SIZE      = 256
-# NUM_EPOCHS      = 5
-# LR              = 0.001
+# BATCH_SIZE      = 128
+# NUM_EPOCHS      = 25
+# LR              = 0.01
 # MOMENTUM        = 0.99
-# TRAIN_SIZE      = 500
-# TEST_SIZE       = 50
+# TRAIN_SIZE      = 512
+# TEST_SIZE       = 128
 # NO_PIXELS       = 224
 
+def save_image(filename, image, bw=False):
+    # print("Dims recieved for printing image: ", image.shape)
+    if bw:
+        image = image*225
+    if type(image) != np.ndarray:
+        cv2.imwrite(filename, image.numpy())
+    else:
+        cv2.imwrite(filename, image)
+        
 """Returns images in a given directory
         Format of return: list of NumPy arrays.
         NumPy arrays are of shape (224,224,3) for images and (224,224) for ground truths 
@@ -62,7 +73,7 @@ def load_images(images, gts, image_dir_path, gt_dir_path, test=False):
         
         # Convert Ground Truth from RGB to 1 channel (Black or White)
         gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY)
-        ret,gt = cv2.threshold(gt,127,255,0)
+        _,gt = cv2.threshold(gt,127,1,0)
         
         # Resize images and ground truths to size 224*224
         img = cv2.resize(img, (NO_PIXELS,NO_PIXELS), interpolation=cv2.INTER_CUBIC)
@@ -154,6 +165,7 @@ class UNET(nn.Module):
         self.d4 = decoder_block(128, 64)
         """ Classifier """
         self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
     
     def forward(self, inputs):
         # For the last few items, the batch size may be smaller than BATCH_SIZE
@@ -173,11 +185,64 @@ class UNET(nn.Module):
         d4 = self.d4(d3, s1)
         """ Classifier """
         x = self.outputs(d4)
-        # x = self.softmax(x)
+        x = self.sigmoid(x)
+        
         """ Reformatting"""
-        outputs = x.reshape(batch_size, NO_PIXELS, NO_PIXELS)
+        outputs = x.clone().reshape(batch_size, NO_PIXELS, NO_PIXELS)
+        
         return outputs
+    
+"""TODO: Exact begrijpen waarom dit wel werkt"""
+class IoULoss(nn.Module):
+    def __init__(self, smooth=1.0):
+        super(IoULoss, self).__init__()
+        self.smooth = smooth
 
+    def forward(self, prediction, target):
+        # Flatten the inputs to 2D tensors (batch_size * height * width, channels)
+        prediction = prediction.view(-1, prediction.size(1))
+        target = target.view(-1, target.size(1))
+
+        # Calculate intersection and union
+        intersection = torch.sum(prediction * target, dim=0)
+        union = torch.sum(prediction + target, dim=0) - intersection
+
+        # Calculate IoU for each channel
+        iou = (intersection + self.smooth) / (union + self.smooth)
+
+        # Average IoU across all channels
+        mean_iou = torch.mean(iou)
+
+        # Return 1 - mean IoU as the loss (to be minimized)
+        loss = 1 - mean_iou
+
+        return loss
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, prediction, target):
+        # Calculate binary cross-entropy loss
+        bce_loss = F.binary_cross_entropy_with_logits(prediction, target, reduction='none')
+
+        # Calculate the modulating factor (alpha * target + (1 - alpha) * (1 - target))^gamma
+        modulating_factor = (self.alpha * target + (1 - self.alpha) * (1 - target)).pow(self.gamma)
+
+        # Calculate the focal loss
+        focal_loss = bce_loss * modulating_factor
+
+        if self.reduction == 'mean':
+            return focal_loss.mean()
+        elif self.reduction == 'sum':
+            return focal_loss.sum()
+        else:
+            return focal_loss
+        
+        
 """Goal is to create a model, load traning and test data and evaltually train and evaluate the model.
 """
 if __name__ == '__main__':
@@ -193,8 +258,14 @@ if __name__ == '__main__':
     
     # Define loss function and optimizer
     # loss_function = nn.CrossEntropyLoss()
-    loss_function = nn.BCELoss()
-    optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+    # loss_function = nn.BCEWithLogitsLoss()
+    # loss_function = nn.L1Loss()
+    # loss_function = nn.BCELoss()
+    loss_function = IoULoss()
+    # loss_function = FocalLoss()
+    
+    # optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+    optimizer = optim.Adam(model.parameters(), lr=LR)
     
     # Store the losses
     train_losses = []
@@ -205,7 +276,21 @@ if __name__ == '__main__':
     train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test, batch_size=BATCH_SIZE, shuffle=False)
             
-    directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output"
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/SGD/Focal_25epoch_test/"
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/SGD/CE_25epoch_test/"
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/SGD/IoU_25epoch_test/"
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/Adam/Focal_25epoch_test/"
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/Adam/CE_25epoch_test/"
+    directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/Adam/IoU_25epoch_test/"
+    os.chdir(directory) 
+    folder = "LR=" + str(LR)# + "_MOMENTUM=" + str(MOMENTUM)
+    os.makedirs(folder)
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/SGD/Focal_25epoch_test/" + folder
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/SGD/CE_25epoch_test/" + folder
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/SGD/IoU_25epoch_test/" + folder
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/Adam/Focal_25epoch_test/" + folder
+    # directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/Adam/CE_25epoch_test/" + folder
+    directory = "/Users/mariekekopmels/Desktop/Uni/MScThesis/Code/Output/Adam/IoU_25epoch_test/" + folder
     os.chdir(directory) 
     
     #Train the model
@@ -225,9 +310,10 @@ if __name__ == '__main__':
             targets = targets.float()
             
             # Train the model
-            optimizer.zero_grad()
             outputs = model(images)
-            
+            # print(f"Output[0] metrics: \nUniques:{outputs[0].unique}\nOne's:{(outputs[0] == 1.0).sum().item()}\nZero's:{(outputs[0] == 0.0).sum().item()}\n225's:{(outputs[0] == 225.0).sum().item()}")
+            # print(f"Output[0] metrics: \nMax:{outputs[0].max()}\nMin:{outputs[0].min()}")
+
             # Store one demo per epoch. Image, model output and ground truth.
             if demo_check == False:
                 demo_image = images[0].permute(1,2,0)
@@ -240,12 +326,16 @@ if __name__ == '__main__':
                 gt = demo_gt.cpu().numpy()
                 
                 filename = "Image_epoch_" + str(epoch) + ".jpg"
-                cv2.imwrite(filename, img)
+                # cv2.imwrite(filename, img)
+                save_image(filename, img)
                 filename = "Output_epoch_" + str(epoch) + ".jpg"
-                cv2.imwrite(filename, out)
+                # cv2.imwrite(filename, out)
+                save_image(filename, out, bw=True)
                 filename = "GT_epoch_" + str(epoch) + ".jpg"
-                cv2.imwrite(filename, gt)
-        
+                # cv2.imwrite(filename, gt)
+                save_image(filename, gt, bw=True)
+    
+
             loss = loss_function(outputs, targets)
             loss.backward()
             optimizer.step()
@@ -268,7 +358,7 @@ if __name__ == '__main__':
         # Print losses TODO: is the /X factor correct? 
         train_losses.append(epoch_train_loss/TRAIN_SIZE)
         test_losses.append(epoch_test_loss/TEST_SIZE)
-        print(f"train loss: {epoch_train_loss/TRAIN_SIZE:.6f} \ntest  loss: {epoch_test_loss/TEST_SIZE:.6f} ")
+        print(f"mean train loss: {epoch_train_loss/TRAIN_SIZE:.6f} \nmean test  loss: {epoch_test_loss/TEST_SIZE:.6f} ")
     
     
     run_time = time.time() - start_time
@@ -280,6 +370,6 @@ if __name__ == '__main__':
     plt.plot(x, test_losses, label='Test Loss', color='red')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
-    plt.ylim(0, 2000)
+    # plt.ylim(0, 2000)
     plt.legend()
     plt.savefig("Loss_plot.jpg")
