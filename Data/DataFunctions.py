2c66449a31fc8faa9fec9d71779a03fb656f3f9e
Marieke Kopmels <marieke@kopmels.com>
2024-01-10 11:28:37 +0100

    added a seed for reproduction purposes

--- Data/DataFunctions.py
+++ Data/DataFunctions.py
@@ -96,7 +96,8 @@ def load_image_data(config):
 """ Splits the data in the passed data loader into a train and validation loader.
 """
 def split_dataset(config, data_loader):
-    train_data, validation_data = random_split(data_loader.dataset, [config.split, 1-config.split])
+    generator = torch.Generator().manual_seed(42)
+    train_data, validation_data = random_split(data_loader.dataset, [config.split, 1-config.split], generator)
     train_loader = DataLoader(train_data, batch_size=config.batch_size, shuffle=True, drop_last=True) 
     validation_loader = DataLoader(validation_data, batch_size=config.batch_size, shuffle=True, drop_last=False) 
     
